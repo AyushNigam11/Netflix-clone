18a369884f7730956dbd18333b56b6d183c02908
Ayush Nigam <95609003+AyushNigam11@users.noreply.github.com>
2024-01-09 17:38:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/login" element={<Login/>}  />
-        <Route exact path="/signup" element={<Signup/>}  />
+        <Route exact path= "/login" element={<Login/>}  />
+        <Route exact path= "/signup" element={<Signup/>}  />
         <Route exact path="/" element={<Netflix/>}  />
       </Routes>
     </BrowserRouter>
